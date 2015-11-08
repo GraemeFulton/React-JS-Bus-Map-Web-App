af6dfac802deb659552548ffc3bbc105f40497d3
Graeme Fulton <graemefulton89@gmail.com>
2015-11-08 22:18:05 +0000

    stylesheet

--- app/departureBoard.jsx
+++ app/departureBoard.jsx
@@ -8,13 +8,18 @@ export default class DepartureBoard extends React.Component {
 
 
   render () {
-      return (  <h4>Departures: {this.props.station}</h4> );
+    var headerStyle = {
+      color: 'rgb(92,193,146)',
+      WebkitTransition: 'all', // note the capital 'W' here
+      msTransition: 'all' // 'ms' is the only lowercase vendor prefix
+    };
+      return (  <h4 style={headerStyle}>Departures: {this.props.station}</h4> );
 
   }
 
   getDepartures(){
 
-    
+
 
   }
 
